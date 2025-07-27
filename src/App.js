87c805656098470bdf99aa409240c7627f10ab08
spiebuildingsolution1 <martynatopyla50@gmail.com>
2025-07-26 21:05:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fullName: 'Joseph wilfred',
+        bio: 'Software developer who loves React.',
+        imgSrc: 'https://cdn.corenexis.com/media/c/5ee3cf&2H&p&c&9p9o.jpg',
+        profession: 'Software Developer'
+      },
+      shows: false,
+      mountedTime: 0
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        mountedTime: prevState.mountedTime + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow() {
+    this.setState(prevState => ({
+      shows: !prevState.shows
+    }));
+  }
+
+  render() {
+    const { person, shows, mountedTime } = this.state;
+
+    return (
+      <div className="App" style={{ padding: '20px' }}>
+        <h1>React Class Component - Profile Toggle</h1>
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide' : 'Show'} Profile
+        </button>
+
+        {shows && (
+          <div style={{ marginTop: '20px', border: '1px solid #ccc', padding: '10px' }}>
+            <img src={person.imgSrc} alt="Profile" style={{ width: '150px' }} />
+            <h2>{person.fullName}</h2>
+            <p><strong>Profession:</strong> {person.profession}</p>
+            <p>{person.bio}</p>
+          </div>
+        )}
+
+        <p style={{ marginTop: '20px' }}>
+          Time since component mounted: {mountedTime} second(s)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
